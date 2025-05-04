926b233c2eec77586e7393afc3da4deb133bea99
Ana <74693547+itsmeanaaa@users.noreply.github.com>
2025-05-04 13:40:11 +0800

    Day 3 of 100 days of coding

---
+++ script.js
@@ -0,0 +1,31 @@
+let score = 0;
+let oneh = 1;
+let twoh = 2;
+let threeh = 3;
+let scoreg = 0;
+
+function addOneScore(){
+    score += oneh
+    document.getElementById("scoreh").innerHTML = score;
+}
+function addTwoScore(){
+    score += twoh
+    document.getElementById("scoreh").innerHTML = score;
+}
+function addThreeScore(){
+    score += threeh
+    document.getElementById("scoreh").innerHTML = score;
+}
+
+function addOneScoreg(){
+    scoreg += oneh
+    document.getElementById("scoreg").innerHTML = scoreg;
+}
+function addTwoScoreg(){
+    scoreg += twoh
+    document.getElementById("scoreg").innerHTML = scoreg;
+}
+function addThreeScoreg(){
+    scoreg += threeh
+    document.getElementById("scoreg").innerHTML = scoreg;
+}
